eb4c4972adce6114a43c18d86f40f573cdd739d4
yskmjp <m@yskm.jp>
2017-10-19 21:26:26 +0900

    first commit

---
+++ junctiontokyo.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="junctiontokyo" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="menu_drink" src="html/menu_drink.png" />
+        <File name="menu_food" src="html/menu_food.png" />
+        <File name="black" src="html/black.jpeg" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,232 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=112\\vct=147\ご|チュウモンを、\rspd=102\\vct=147\どうぞーーーッッ、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=112\\vct=147\ご|チュウモンを、\rspd=102\\vct=147\どうぞーーーッッ、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=116\\vct=142\ハイッッ、\rspd=108\\vct=141\かしこまりましたーーッ、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=116\\vct=142\ハイッッ、\rspd=108\\vct=141\かしこまりましたーーッ、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=146\おかわりは\rspd=110\\vct=140\いかがですか？</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=146\おかわりは\rspd=110\\vct=140\いかがですか？</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=145\本日のオススメは、\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\本日のオススメは、\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=145\本日のオススメは、\rspd=110\\vct=152\こちらです</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\本日のオススメは、\rspd=110\\vct=152\こちらです</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=142\お客様へのオススメは、\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=142\お客様へのオススメは、\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=145\\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=112\\vct=147\ご|チュウモンを、\rspd=102\\vct=147\どうぞーーーッッ、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=112\\vct=147\ご|チュウモンを、\rspd=102\\vct=147\どうぞーーーッッ、</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=110\\vct=146\おかわりは\rspd=110\\vct=140\いかがですか？</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=110\\vct=146\おかわりは\rspd=110\\vct=140\いかがですか？</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=116\\vct=142\ハイッッ、\rspd=108\\vct=141\かしこまりましたーーッ、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=116\\vct=142\ハイッッ、\rspd=108\\vct=141\かしこまりましたーーッ、</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=110\\vct=142\お客様へのオススメは、\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=110\\vct=142\お客様へのオススメは、\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=146\おかわりは\rspd=110\\vct=140\いかがですか？</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=146\おかわりは\rspd=110\\vct=140\いかがですか？</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=112\\vct=147\ご|チュウモンを、\rspd=102\\vct=147\どうぞーーーッッ、</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=112\\vct=147\ご|チュウモンを、\rspd=102\\vct=147\どうぞーーーッッ、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=145\本日のオススメは、\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\本日のオススメは、\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>\rspd=116\\vct=142\ハイッッ、\rspd=108\\vct=141\かしこまりましたーーッ、</source>
+            <comment>Text</comment>
+            <translation type="unfinished">\rspd=116\\vct=142\ハイッッ、\rspd=108\\vct=141\かしこまりましたーーッ、</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=142\お客様へのオススメは、\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=142\お客様へのオススメは、\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>お会計は８０００円です</source>
+            <comment>Text</comment>
+            <translation type="unfinished">お会計は８０００円です</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>\rspd=110\\vct=145\\rspd=110\\vct=152\こちらです、</source>
+            <comment>Text</comment>
+            <translation type="obsolete">\rspd=110\\vct=145\\rspd=110\\vct=152\こちらです、</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>今回楽しんでいただけたみたいで良かったです！</source>
+            <comment>Text</comment>
+            <translation type="unfinished">今回楽しんでいただけたみたいで良かったです！</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>こちらは当店のクーポンです、次回ご来店時にお使いください</source>
+            <comment>Text</comment>
+            <translation type="unfinished">こちらは当店のクーポンです、次回ご来店時にお使いください</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>ご利用ありがとうございました、またお越しください</source>
+            <comment>Text</comment>
+            <translation type="unfinished">ご利用ありがとうございました、またお越しください</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (8)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>送信</source>
+            <comment>Text</comment>
+            <translation type="obsolete">送信</translation>
+        </message>
+    </context>
+</TS>
